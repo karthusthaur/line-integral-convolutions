de9218994ab0133b373ee88053eed3fe82acf09e
neco kriel <necokriel@gmail.com>
2024-12-27 18:57:25 +1100

    add: other example vector fields

--- src/line_integral_convolutions/fields.py
+++ src/line_integral_convolutions/fields.py
@@ -34,4 +34,59 @@ def vfield_lotka_volterra(size: int):
     }
 
 
+def vfield_flowers(size: int):
+    bounds_rows = (-10, 10)
+    bounds_cols = (-10, 10)
+    coords_row = np.linspace(bounds_rows[0], bounds_rows[1], size)
+    coords_col = np.linspace(bounds_cols[0], bounds_cols[1], size)
+    mg_x, mg_y = np.meshgrid(coords_col, coords_row, indexing="xy")
+    vcomp_rows = np.cos(mg_x / 2)
+    vcomp_cols = np.cos(mg_y / 2)
+    vfield = np.array([vcomp_rows, vcomp_cols])
+    return {
+        "vfield": vfield,
+        "streamlength": size // 4,
+        "num_rows": size,
+        "num_cols": size,
+        "bounds_rows": bounds_rows,
+        "bounds_cols": bounds_cols,
+    }
+
+def vfield_circles(size: int):
+    bounds_rows = (-10, 10)
+    bounds_cols = (-10, 10)
+    coords_row = np.linspace(bounds_rows[0], bounds_rows[1], size)
+    coords_col = np.linspace(bounds_cols[0], bounds_cols[1], size)
+    mg_x, mg_y = np.meshgrid(coords_col, coords_row, indexing="xy")
+    vcomp_rows = np.cos(mg_y / 2)
+    vcomp_cols = np.cos(mg_x / 2)
+    vfield = np.array([vcomp_rows, vcomp_cols])
+    return {
+        "vfield": vfield,
+        "streamlength": size // 4,
+        "num_rows": size,
+        "num_cols": size,
+        "bounds_rows": bounds_rows,
+        "bounds_cols": bounds_cols,
+    }
+
+def vfield_swirls(size: int):
+    bounds_rows = (-10, 10)
+    bounds_cols = (-10, 10)
+    coords_row = np.linspace(bounds_rows[0], bounds_rows[1], size)
+    coords_col = np.linspace(bounds_cols[0], bounds_cols[1], size)
+    mg_x, mg_y = np.meshgrid(coords_col, coords_row, indexing="xy")
+    vcomp_rows = np.sin((mg_y + mg_x) / 3)
+    vcomp_cols = np.cos((mg_x - mg_y) / 3)
+    vfield = np.array([vcomp_rows, vcomp_cols])
+    return {
+        "vfield": vfield,
+        "streamlength": size // 3,
+        "num_rows": size,
+        "num_cols": size,
+        "bounds_rows": bounds_rows,
+        "bounds_cols": bounds_cols,
+    }
+
+
 ## END OF EXAMPLE VECTOR FIELDS
